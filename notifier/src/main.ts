0ae264015d75ae2c98c07784a559181ed700f410
Isra Nebot <israel.nebot@gmail.com>
2022-12-14 10:14:01 +0100

    Add new Notifier block decoder

--- notifier/src/main.ts
+++ notifier/src/main.ts
@@ -2,7 +2,7 @@ import { NetworkProvider } from './common/network.provider';
 import { Configuration } from './config/Configuration';
 import { Network } from '@hyperledger/fabric-gateway';
 import { WebSocketServer } from 'ws';
-import { BlockDecoder } from './common/block.decoder';
+import { BlockDecoder } from './common/block/BlockDecoder';
 
 async function start() {
   console.log('Initializing WebSocket Server...');
@@ -16,8 +16,8 @@ async function start() {
 
     console.log('Listening for Blockchain events.');
     for await (const block of blockEvents) {
-      const decodedBlock = new BlockDecoder(block).decode();
-      console.log(`Received new block. Block number ${decodedBlock.blockNum}`);
+      const decodedBlock = BlockDecoder.decode(block);
+      console.log(JSON.stringify(decodedBlock, null, 2));
 
       ws.send(JSON.stringify(decodedBlock, null, 2));
     }
